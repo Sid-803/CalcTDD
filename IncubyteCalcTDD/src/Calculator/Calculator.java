7930633a5b70292ff982045024ce29973d623070
Siddhartha <siddharthchauhan888@gmail.com>
2022-06-21 00:18:15 +0530

    Removed un necessary getter and Setter methods from Calculator.java class

--- IncubyteCalcTDD/src/Calculator/Calculator.java
+++ IncubyteCalcTDD/src/Calculator/Calculator.java
@@ -5,7 +5,6 @@ public class Calculator {
 	private String string = "";
 	private String a1 = "";
 	private String a2 = "";
-	//private int k=0;
 	private int sum =0;
 	public int i = 0;
 	
@@ -41,39 +40,12 @@ public class Calculator {
 		return string;
 	}
 
-	public void setString(String string) {
-		this.string = string;
-	}
-
 	public String getA1() {
 		return a1;
-	}
-
-	public void setA1(String a1) {
-		this.a1 = a1;
 	}
 
 	public String getA2() {
 		return a2;
 	}
 
-	public void setA2(String a2) {
-		this.a2 = a2;
-	}
-
-	//public int getK() {
-	//	return k;
-	//}
-
-	//public void setK(int k) {
-	//	this.k = k;
-	//}
-
-	public int getSum() {
-		return sum;
-	}
-
-	public void setSum(int sum) {
-		this.sum = sum;
-	}
 }
