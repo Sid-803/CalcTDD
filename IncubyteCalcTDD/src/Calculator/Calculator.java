4a13cfc3d24d56c3e2c71a5d25441ebfb4304a5f
Siddhartha <siddharthchauhan888@gmail.com>
2022-06-20 20:51:06 +0530

    Create an attribute String string = null; Created a Method "Add" parameter1 = (String string) Explicit return type is integer

--- IncubyteCalcTDD/src/Calculator/Calculator.java
+++ IncubyteCalcTDD/src/Calculator/Calculator.java
@@ -1,5 +1,6 @@
 package Calculator;
 
-public class Calculator {
 
+public class Calculator {
+	
 }
--- IncubyteCalcTDD/src/Test/Test.java
+++ IncubyteCalcTDD/src/Test/Test.java
@@ -1,7 +1,14 @@
 package Test;
-
+//Basic Architecture of Calculator POJO class.
+//1. Attribute 1 for POJO class is-string of String Datatype.
+//2. It will contain a method which accepts String as parameter. MethodName "Add".
 public class Test {
-
+	String string = null;
+	
+	public int Add(String string) {
+		int result=0;
+		return result;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
