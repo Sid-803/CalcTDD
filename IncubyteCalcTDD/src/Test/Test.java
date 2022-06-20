5c06ce24ba43f1399912d997000e44965a129f9b
Siddhartha <siddharthchauhan888@gmail.com>
2022-06-20 22:19:56 +0530

    Replace method "add" Package - Test, CLass - Test.java Parameters =2 (String a1, String a2) explicit return type = Integer Description: It takes two strings as input. Convert those string into Integer returns sum of both integers

--- IncubyteCalcTDD/src/Test/Test.java
+++ IncubyteCalcTDD/src/Test/Test.java
@@ -3,15 +3,12 @@ package Test;
 //1. Attribute 1 for POJO class is-string of String Datatype.
 //2. It will contain a method which accepts String as parameter. MethodName "Add".
 public class Test {
-	static String string = null;
-	static String a1 = null;
-	static String a2 = null;
-	
-	public static int Add(String string) {
-		int result=0;
-		return result;
-	}
-	
+	static String string = "";
+	static String a1 = "";
+	static String a2 = "";
+	static int k=0;
+	static int sum =0;
+		
 	public static int searchCommas(String s1, char key) {
 		for(int i = 0; i<s1.length();i++) {
 			if(s1.charAt(i)==key) {
@@ -21,23 +18,28 @@ public class Test {
 		return -1;
 	}
 	
-	public void split(String s1) {
-		int k = searchCommas(s1, ',');
+	public static void split(String s2) {
+		k = searchCommas(s2, ',');
 		for(int i = 0; i<k;i++) {
-			for(int j = k+1; j<s1.length();j++) {
-				char a = s1.charAt(i);
-				char b = s1.charAt(j);
+			for(int j = k+1; j<s2.length();j++) {
+				char a = s2.charAt(i);
+				char b = s2.charAt(j);
 				a1 = a1 +a;
 				a2 = a2 +b;
-			}
-		}
+			}	
+		}	
+	}
+	
+	public static int add(String a1, String a2) {
+		int k1 = Integer.valueOf(a1);
+		int k2 = Integer.valueOf(a2);
+		sum = k1 + k2;
+		return sum;
 	}
 	
 	public static void main(String[] args) {
-
-			string = "1,2,2";
-			System.out.println(searchCommas(string,','));	
-			
+			string = "1,2";
+			split(string);
+			System.out.println(add(a1,a2));	
 	}
-
 }
