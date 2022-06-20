cb883f4f1c784253f8f0a0b2d55c3984432153c6
Siddhartha <siddharthchauhan888@gmail.com>
2022-06-20 21:46:32 +0530

    New Method - Package(Test) and Class(Test.java) Name= split Parameters = 1, String s1 No return type(void) Description Takes a string, search for comma and divides the string into two sub strings from comma. Makes use of Method2(searchCommas)

--- IncubyteCalcTDD/src/Test/Test.java
+++ IncubyteCalcTDD/src/Test/Test.java
@@ -4,23 +4,40 @@ package Test;
 //2. It will contain a method which accepts String as parameter. MethodName "Add".
 public class Test {
 	static String string = null;
+	static String a1 = null;
+	static String a2 = null;
 	
 	public static int Add(String string) {
 		int result=0;
 		return result;
 	}
 	
-	public int searchCommas(String a3, char key) {
-		for(int i = 0; i<a3.length();i++) {
-			if(a3.charAt(i)==key) {
+	public static int searchCommas(String s1, char key) {
+		for(int i = 0; i<s1.length();i++) {
+			if(s1.charAt(i)==key) {
 				return i;
 			}
 		}
 		return -1;
 	}
 	
+	public void split(String s1) {
+		int k = searchCommas(s1, ',');
+		for(int i = 0; i<k;i++) {
+			for(int j = k+1; j<s1.length();j++) {
+				char a = s1.charAt(i);
+				char b = s1.charAt(j);
+				a1 = a1 +a;
+				a2 = a2 +b;
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
-		string = "";
-		System.out.println(Add(string));	}
+
+			string = "1,2,2";
+			System.out.println(searchCommas(string,','));	
+			
+	}
 
 }
