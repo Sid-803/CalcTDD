9a46d3e197a7e4fbd752f25f13977c758857b9c9
Siddhartha <siddharthchauhan888@gmail.com>
2022-06-20 20:57:46 +0530

    Assigned string object as blank in main Printed Method Add in main Normal execution

--- IncubyteCalcTDD/src/Test/Test.java
+++ IncubyteCalcTDD/src/Test/Test.java
@@ -3,15 +3,14 @@ package Test;
 //1. Attribute 1 for POJO class is-string of String Datatype.
 //2. It will contain a method which accepts String as parameter. MethodName "Add".
 public class Test {
-	String string = null;
+	static String string = null;
 	
-	public int Add(String string) {
+	public static int Add(String string) {
 		int result=0;
 		return result;
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		string = "";
+		System.out.println(Add(string));	}
 
 }
