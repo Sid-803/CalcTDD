13ac37eaa3fa405e481d60019ba011ab65602351
Siddhartha <siddharthchauhan888@gmail.com>
2022-06-21 17:18:38 +0530

    Main function in Test Class is free of any condition.
    All logic lies in Calculator class
    
    In main function of Test class.
    
    There is only instantiation of Calculator class, calling add method of
    Calculator class and Exception handling and output(sum or error)

--- IncubyteCalcTDD/src/Calculator/Calculator.java
+++ IncubyteCalcTDD/src/Calculator/Calculator.java
@@ -12,11 +12,36 @@ public class Calculator {
 		this.string = string;
 	}
 	
+	//Algo for this method
+		//1. Check if string is empty, if not check for delimiter.
+		//2. Delimiter method removes delimiting componenets(if exist). Output is Integer figures and commas 
+		//3. Assign attribute i value where string has comma.
+		//4. Splits the single string(string) into two sub-string(a1,a2) separated by comma(pre comma=a2 and post comma =a2)  based on i value
+		//5. Converts both sub strings(a1,a2) into integers(k1,k2)
+		//6. Returns sum of both Integers
+		//Exceptions: Throws NumberFormatException if k1 or k2 is anything other than Integer.
+		public int add() {
+			if(!string.isEmpty()) {
+				string=delimiter(string);
+			}
+			
+			searchCommas(string);
+			for(int j = 0; j<i;j++) {
+				a1 = a1 + string.charAt(j);
+			}
+			for(int k = i+1; k<string.length();k++) {		
+				a2 = a2 + string.charAt(k);
+			}
+			k1= Integer.valueOf(a1);
+			k2= Integer.valueOf(a2);
+			return k1+k2;
+		}
+	
+	//Simplifies string input of format "//;\n1,2" into "1,2"
 	public String delimiter(String s) {
 		s=s.replace("\n","");
 		if(s.charAt(0)=='/') {
 			String s0="";
-			
 			for(int k=3;k<s.length();k++) {
 				if(s.charAt(k)==';' || s.charAt(k)==' '|| s.charAt(k)=='.') {
 					s0=s0+',';
@@ -29,7 +54,8 @@ public class Calculator {
 			return s;
 		}
 	}
-		
+	
+	//Assign attribute i, the value of index where comma exist
 	public void searchCommas(String s1) {
 		for(int k =0; k<s1.length();k++) {
 			if(s1.charAt(k)==',')
@@ -37,18 +63,7 @@ public class Calculator {
 		}
 	}
 	
-	public int add(String s2) {
-		searchCommas(s2);
-		for(int j = 0; j<i;j++) {
-			a1 = a1 + s2.charAt(j);
-		}
-		for(int k = i+1; k<s2.length();k++) {		
-			a2 = a2 + s2.charAt(k);
-		}
-		k1= Integer.valueOf(a1);
-		k2= Integer.valueOf(a2);
-		return k1+k2;
-	}
+	
 
 	public String getString() {
 		return string;
--- IncubyteCalcTDD/src/Test/Test.java
+++ IncubyteCalcTDD/src/Test/Test.java
@@ -5,32 +5,44 @@ import Calculator.Calculator;
 
 public class Test {
 	
-	
 	public static void main(String[] args) {
-		String s = "-1,-2";
+		//Input String
+		String s = "";
+		
+		//Object of Calculator POJO class
 		Calculator calculator = null;
 		int sum=0;
 		
+		//Try-catch to handle if non integer value or negative number exist
 		try {
 			calculator = new Calculator(s);
-			if(!s.isEmpty()) {
-				calculator.setString(calculator.delimiter(s));
-			}
-			sum = calculator.add(calculator.getString());
+			
+			//Read Comment in Calculator.java - add method
+			sum = calculator.add();
+			
+			//Custome exception is thrown if k1 and/or negative after Calculator.add method is called.
 			if(calculator.getK1() <0 || calculator.getK2()<0) 
 				throw new NegativeNumberException("Error Message: Addition not possible as negative number exist");
-				
+		
+		//Handles if Invalid Integer format exist in String
 		}catch(NumberFormatException e) {
 			System.out.println("Error Message: NumberFormat Exception: invalid input string");
 			System.exit(0);
+		
+		//Handles if negative numbers exist in String
 		}catch(NegativeNumberException ex) {
 			System.out.println(ex.getMessage());
 			
-	}finally{
+		}finally{
+			//Prints negative numbers in string if both numbers in string are negative after handling the respective exception
 			if(calculator.getK1() <0  & calculator.getK2()<0) 
 				System.out.println("i.e " + calculator.getK1() + "," + calculator.getK2());
+			
+			//Doesnt calculate sum if anyone number in string is negative
 			if (calculator.getK1() <0 || calculator.getK2() <0)  
 				System.out.println("The sum is unavailable.");
+			
+			//Prints Sum in accepted scenerios
 			else
 				System.out.println("The sum is: " + sum);
 		}
